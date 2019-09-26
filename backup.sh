10aed2bb30a844ccd1a9055a723888f95281d6c7
xanadu <xanadu@Arch>
2019-09-26 23:31:35 +0200

    remove unused/broken scripts and other junk that doesn't belong

--- backup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# A Script to run as a cronjob, backs up music/soundtracks/documents to different drives for redundancy
-# Also makes lists of all files so they can be downloaded again if lost.
-
-echo starting backup..
