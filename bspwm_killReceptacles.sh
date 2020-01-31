9d13d0d620f45f04027088bed5c2092cb154bb72
xanadu <xanadu@Arch>
2020-01-31 19:45:50 +0100

    CLEANUP
    A bunch of files that weren't used anymore are gone now.
    The really short scripts have been moved to ~/.bashrc

--- bspwm_killReceptacles.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#for x in $(bspc query -N -n '.leaf.!window')
-for x in $(bspc query -N -n '.leaf.sticky')
-do
-	bspc node "$x" -k
-done
--- playHiFi.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-arecord -D "front:CARD=Generic,DEV=0" -f S16_LE -r 48000 -c2 >(aplay -D "pulse" -f s16_LE -r 48000 -c2)
