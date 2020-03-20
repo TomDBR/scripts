4a8e764b60d1f1d4485594e7bf9458d1047c3897
xanadu <canardo@cock.li>
2020-03-20 09:39:37 +0100

    allow exiting script early without making any changes

--- URxvt_font_switcher.sh
+++ URxvt_font_switcher.sh
@@ -2,10 +2,12 @@
 # see `man 7 urxvt`
 # https://emergent.unpythonic.net/01297173664 tmux related stuff
 
-font_style="$(fc-list :lang=en family style | fzf)"
+font_style="$(fc-list :lang=en:style=Regular family style | fzf)"
+[[ $font_style == "" ]] && exit 1
+size="$(seq 6 2 40 | fzf)"
+[[ $size == "" ]] && exit 1
 font="$(echo "$font_style" | cut -d":" -f1)"
 style="$(echo "$font_style" | cut -d":" -f2)"
-size="$(seq 6 2 40 | fzf)"
 case "$TERM" in
 	tmux*)  # this doesn't WORK, it does work in screen though
 		sleep .1; 
