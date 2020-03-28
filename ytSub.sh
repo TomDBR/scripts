e86929fb35b206aac74fcd30b7407317dfaf5289
xanadu <xanadu@MJ12>
2020-03-28 12:59:56 +0100

    ytsub cleanup

--- ytSub.sh
+++ ytSub.sh
@@ -12,11 +12,13 @@ do
 		*)
 			mapfile -t < <(echo $line | tr ' ' '\n') list 
 			[[ ${#list[@]} -eq 2 ]] || continue # ignore line if we didn't have 2 matches
-			youtube-dl --add-metadata --dateafter now-1week -i -o ${list[0]}/%\(title\)s.%\(ext\)s ${list[1]} &>/tmp/ytdl_tmp &
+			set -x
+			youtube-dl --add-metadata --dateafter now-1week -i -o ${list[0]}/%\(title\)s.%\(ext\)s ${list[1]} 2>&1 | tee /tmp/ytdl_tmp &
+			set +x
 			id=$!
 			while kill -0 $id 2>/dev/null
 			do 
-				grep -q "upload date is not in range" /tmp/ytdl_tmp || kill $id
+				grep -q "upload date is not in range|already been recorded in archive" /tmp/ytdl_tmp && kill $id
 				sleep 1
 			done &>/dev/null
 			;;
@@ -26,9 +28,11 @@ done < ~/.cache/ytsubs
 # was used to tag videos that could be removed, but nowadays this script is only used for archival purposes, so comment out the next part
 while read -r video
 do
-    grep -q "$video" /home/xanadu/.config/ranger/tagged && rm "$video"
-	sed -i "|$line|d" /home/xanadu/.config/ranger/tagged
-done < <(find ~/Videos/Youtube/Subscriptions -type -f -iname "*.mkv")
+    if grep -q "$video" /home/xanadu/.config/ranger/tagged; then
+		rm "$video" 
+		sed -i "|$video|d" /home/xanadu/.config/ranger/tagged
+	fi
+done < <(find ~/Videos/Youtube/Subscriptions -type f -iname "*.mkv")
 	
 # delete empty directories
 find ~/Videos/Youtube/Subscriptions -type d -empty -delete
