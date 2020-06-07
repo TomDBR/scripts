0c53b8e6877268aa1f532d42a72982fd1975835d
xanadu <canardo@cock.li>
2020-06-07 13:23:45 +0200

    bspwm_updateMonitors
    kill bar after changing monitors so it displays correctly.
    use more intuitive names
    some small changes and TODO stuff

--- bspwm_updateMonitors.sh
+++ bspwm_updateMonitors.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
-declare -a monitors=( $(xrandr | grep 'connected' | cut -d" " -f1,2 | tr " " ":") )
-declare -a connectedMonitors=( $(xrandr | grep 'connected' | cut -d" " -f1) )
+# TODO: allow choice of primary monitor
+# TODO: custom monitor position
+declare -a outputStatus=( $(xrandr | grep 'connected' | cut -d" " -f1,2 | tr " " ":") ) # eg. LVDS-1:connected
+declare -a outputs=( $(xrandr | grep 'connected' | cut -d" " -f1) ) # eg. LVDS-1
 
 workspaceNames="I II III IV V VI VII VIII IX X" # needs to be separated by spaces!
 wspaceNameArr=( $(echo $workspaceNames | tr " " "\n") )
-LVDS1="--output LVDS-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal" #main monitor, settings can be hardcoded
+LVDS1="--output LVDS-1 --mode 1920x1080 --pos 2560x0 --rotate normal" #main monitor, settings can be hardcoded
 cmd="xrandr"
 
 function querySettings() {
 	xD="$1"
 	mode="$(xrandr --query | awk "/$xD/{p=1;next;print} p&&/^[^ ]/{p=0};p" | sed 's/ *//' | cut -d" " -f1 | rofi -dmenu)"
-	cmd="$cmd --output "$xD" --mode "$mode" --pos 1920x0 --rotate normal"
+	cmd="$cmd --output "$xD" --primary --mode "$mode" --pos 0x0 --rotate normal"
 }
 
-for monitor in "${monitors[@]}"; do
+for monitor in "${outputStatus[@]}"; do
 	output="$(echo $monitor | cut -d":" -f1)"
 	status="$(echo $monitor | cut -d":" -f2)"
 	case "$status" in
@@ -30,8 +32,8 @@ for monitor in "${monitors[@]}"; do
 			;;
 		"disconnected")
 			cmd="$cmd --output $output --off"
-			for i in "${!connectedMonitors[@]}"; do
-				[[ "${connectedMonitors[i]}" == "$output" ]] && unset 'connectedMonitors[i]'
+			for i in "${!outputs[@]}"; do
+				[[ "${outputs[i]}" == "$output" ]] && unset 'outputs[i]'
 			done
 			[[ $(bspc query -M --names | grep "$output") ]] && bspc monitor "$output" -r
 			;;
@@ -39,10 +41,10 @@ for monitor in "${monitors[@]}"; do
 done
 
 $cmd
-wspacePerMon=$(( ${#wspaceNameArr[@]} / ${#connectedMonitors[@]} )) # no idea what happens if this is a floating point ;3
+wspacePerMon=$(( ${#wspaceNameArr[@]} / ${#outputs[@]} )) # no idea what happens if this is a floating point ;3
 
 let iter=0
-for conMon in "${connectedMonitors[@]}"; do
+for conMon in "${outputs[@]}"; do
 	if (( iter == 0 )); then
 		wspaces="$(echo $workspaceNames | cut -d" " -f$(seq -s , $(( (iter+1) * $wspacePerMon)) ))"
 	else
@@ -52,3 +54,4 @@ for conMon in "${connectedMonitors[@]}"; do
 	(( iter++ ))
 done
 
+pkill bar && nohup bar &>/dev/null &
