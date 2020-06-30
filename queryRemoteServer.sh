314acc1a09d5e17de5f5f918d560490228933bcc
TomDBR <canardo@cock.li>
2020-06-30 13:22:41 +0200

    set correct PORT on remote host, exit early on failure

--- queryRemoteServer.sh
+++ queryRemoteServer.sh
@@ -3,23 +3,21 @@ queryRemoteServer() {
 	(( $# == 0 )) && echo "Illegal number of parameters" && return 1
 
 	program="$1"; shift 
-	args="$@"
+	args="$*"
 	MAC="$(ip neigh | grep "$(ip route | grep default | cut -d" " -f3)" | cut -d" " -f5)"
+	HOST_NAME='localhost'
+	export MPD_PORT=6600
 
-	if grep -q "$ssid" ~/.local/share/trusted_MAC; then
-		if ! pgrep mpd &>/dev/null; then
-			if curl --http0.9 http://MJ12:6600 &>/dev/null; then
-				export MPD_PORT=6600
-			else
-				export MPD_PORT=6605
-			fi
-			/usr/bin/$program -h MJ12 -p $MPD_PORT $args
-		else
-			/usr/bin/$program -h localhost $args
-		fi
-	else
-		echo "not connected to home network, aborting..."
+	if ! command -v "$program" &>/dev/null; then
+		echo "Program $program not found!" && return 1
 	fi
+	if ! grep -q "$MAC" ~/.local/share/trusted_MAC; then
+		echo "not connected to home network, aborting..." && return 1
+	fi
+	pgrep mpd &>/dev/null || HOST_NAME="MJ12"
+	curl -s http://"$HOST_NAME":"$MPD_PORT" &>/dev/null || export MPD_PORT=6605
+		
+	/usr/bin/"$program" -h "$HOST_NAME" -p "$MPD_PORT" "$args"
 }
 
 queryRemoteServer "$@"
