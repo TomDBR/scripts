0dded1c0affc1cee6776577ab2a76cc9bb438f27
TomDBR <canardo@cock.li>
2020-07-07 13:09:23 +0200

    ignore tun devices

--- queryRemoteServer.sh
+++ queryRemoteServer.sh
@@ -4,9 +4,12 @@ queryRemoteServer() {
 
 	program="$1"; shift 
 	args="$*"
-	MAC="$(ip neigh | grep "$(ip route | grep default | cut -d" " -f3)" | cut -d" " -f5)"
+	# TODO multiple tun devices? probably unlikely
+	tun="$(ip tuntap list 2>/dev/null | cut -d':' -f1 | head -n 1)"
+	gw="$(ip route show default | grep -v $tun | cut -d " " -f3)"
+	MAC="$(ip neigh | grep "$gw" | cut -d" " -f5)"
 	HOST_NAME='localhost'
-	export MPD_PORT=6600
+	MPD_PORT=6600
 
 	if ! command -v "$program" &>/dev/null; then
 		echo "Program $program not found!" && return 1
@@ -15,7 +18,7 @@ queryRemoteServer() {
 		echo "not connected to home network, aborting..." && return 1
 	fi
 	pgrep mpd &>/dev/null || HOST_NAME="MJ12"
-	curl -s http://"$HOST_NAME":"$MPD_PORT" &>/dev/null || export MPD_PORT=6605
+	curl -s http://"$HOST_NAME":"$MPD_PORT" &>/dev/null || MPD_PORT=6605
 		
 	/usr/bin/"$program" -h "$HOST_NAME" -p "$MPD_PORT" $args
 }
